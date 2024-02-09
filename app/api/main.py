4468d3359be404168e06f25c472b129ca04dddb8
FeMarzani <marzaniarrieche@gmail.com>
2024-02-08 23:25:36 -0300

    :sparkles: feat: implementação de uma api flask para receber como POST um video/audio e fazer speech to text e text generation

--- app/api/main.py
+++ app/api/main.py
@@ -1,28 +1,72 @@
 # Biblioteca Importada
 import requests
+import os
 from pathlib import Path
 from openai import OpenAI
 from flask import Flask, request, jsonify, send_file
 
 # Cliente OpenAI
-client = OpenAI(api_key="COLOCAR A CHAVE DA API AQUI.")
+client = OpenAI("COLOCAR CHAVE DA API AQUI")
 
-audio_file= open("teste.mp4", "rb")
-transcript = client.audio.transcriptions.create(
-  model="whisper-1", 
-  file=audio_file,
-  response_format="text"
-)
+app = Flask(__name__)
 
-type_of_generation = f'Do que este texto se trata: {transcript}'
+# Definindo uma função para pegar o tamanho de um arquivo em MB.
+def tamanho(file_path):
+    return os.path.getsize(file_path) / (1024 * 1024)
 
-geracao = client.completions.create(
-  model="gpt-3.5-turbo-instruct",
-  prompt=type_of_generation
-)
+@app.route('/resumo', methods=['POST'])
+def resumo():
+    
 
-print(type_of_generation)
+    print(request.files)
 
-geracao = geracao.choices[0].text
+    # Verificando se foi enviado um video na requisição:
+    if 'file' not in request.files:
+        return jsonify({'error': 'Nenhum arquivo enviado'}), 400
 
-print(geracao)
+    file = request.files['file']
+
+    # Verificando o nome do video corretamente:
+    if file.filename == '':
+        return jsonify({'error': 'Nome de arquivo vazio'}), 400
+
+    # Verificando o tamanho do arquivo:
+
+    # Salvando temporariamente o arquivo em um file path
+    file_path = 'temp_video.mp4'
+    file.save(file_path)
+
+    file_size_mb = tamanho(file_path)
+    if file_size_mb > 25:
+        return jsonify({'error': 'Arquivo é maior que 25 MB'}), 400
+
+
+    try:
+
+        audio_file = open(file_path, "rb")
+
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file,
+            response_format="text"
+        )
+
+        # Fechando o arquivo posterior a transcrição
+        audio_file.close()
+
+        # Gerando o resumo:
+        type_of_generation = f'Do que este texto se trata: {transcript}'
+
+        geracao = client.completions.create(
+            model="gpt-3.5-turbo-instruct",
+            prompt=type_of_generation
+        )
+
+        # Retorna o resumo gerado
+        return jsonify({'resumo': geracao.choices[0].text}), 200
+
+    except Exception as e:
+        return jsonify({'error': f'Erro ao processar o arquivo de vídeo: {str(e)}'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
